c68c0ad1f2f1cfb8718da45318a950727a2adf43
Danny Hadley <dadley@sizethreestudios.com>
2014-08-11 22:10:11 -0400

    adding more properties to device model

--- api/models/Device.js
+++ api/models/Device.js
@@ -7,6 +7,14 @@ module.exports = {
       required: true
     },
 
+    hostname: {
+      type: 'string'
+    },
+
+    status: {
+      type: 'boolean'
+    },
+
     ip_addr: {
       type: 'string',
       regex: /\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b/
