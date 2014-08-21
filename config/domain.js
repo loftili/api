04ee8c4421106fce7787eeb70a652af558c27a76
Danny Hadley <dadley@sizethreestudios.com>
2014-08-20 20:07:09 -0400

    updating domain list

--- config/domain.js
+++ config/domain.js
@@ -1,11 +1,11 @@
 module.exports = {
 
   allowed_origins: [
-    'http://local-ui.loftili.com',
+    'http://loftili.com',
     'http://app.loftili.com',
-    'http://beta.app.loftili.com'
+    'http://beta.app.loftili.com',
+    'http://local-ui.loftili.com'
   ],
-
   subdomain_parent: 'loftili.com'
 
 };
