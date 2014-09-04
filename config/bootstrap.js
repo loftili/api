258648dcd6e0152aa9be4dfd2e32192f069de2c7
Danny Hadley <dadley@sizethreestudios.com>
2014-09-04 19:41:18 -0400

    [LFTILI-27] adding an additional user to sailsjs bootstrap/seeding

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -1,10 +1,19 @@
 module.exports.bootstrap = function(cb) {
+  var completed = 0,
+      errored = false;
 
   function added(err, user) {
-    if(err) 
-      cb(err);
+    if(errored)
+      return false;
 
-    cb();
+    if(err) {
+      errored = true;
+      return cb(err);
+    }
+
+    completed++;
+    if(completed === 2)
+      cb();
   }
 
   var danny = User.create({
@@ -14,5 +23,15 @@ module.exports.bootstrap = function(cb) {
     last_name: 'hadley',
     username: 'dadleyy'
   }, added);
+
+
+  var test1 = User.create({
+    email: 'test@loftili.com',
+    password: 'password',
+    first_name: 'test',
+    last_name: 'test',
+    username: 'test1'
+  }, added);
+
 
 };
